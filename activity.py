a025b37829094c3bb778b02314314042bd4be895
Walter Bender <walter@sugarlabs.org>
2013-07-14 21:58:18 -0400

    update icon in .icons

--- activity.py
+++ activity.py
@@ -21,6 +21,9 @@
 #  MA 02110-1301, USA.
 #
 #
+import logging
+import os
+import subprocess
 
 from sugar3.activity import activity
 from sugar3.graphics.toolbarbox import ToolbarBox
@@ -29,11 +32,14 @@ from sugar3.graphics.toolbutton import ToolButton
 from sugar3.activity.widgets import _create_activity_icon as ActivityIcon
 from sugar3.graphics.alert import NotifyAlert
 
-from gi.repository import GConf
 from gi.repository import Gtk
+
 from Widgets import XoIcon
 
 from gettext import gettext as _
+
+SUGAR_ICON_PATH = 'sugar/scalable/device'
+DEFAULT_ICON = 'computer-xo'
 
 
 class IconChangeActivity(activity.Activity):
@@ -66,6 +72,14 @@ class IconChangeActivity(activity.Activity):
         self.toolbar.insert(separator, -1)
         self.toolbar.insert(stop_button, -1)
 
+        # We may have to add an comuter-xo icon to ~/.icons
+        root_path = os.path.join(os.path.expanduser('~'), '.icons')
+        if not os.path.exists(os.path.join(root_path, DEFAULT_ICON + '.svg')):
+            from_path = os.path.join(activity.get_bundle_path(),
+                                     'icons', DEFAULT_ICON + '.svg') 
+            command = ['cp', from_path, root_path]
+            subprocess.check_output(command)
+
         self.canvas = XoIcon(activity.get_bundle_path())
 
         self.set_toolbar_box(self.toolbar_box)
@@ -77,9 +91,15 @@ class IconChangeActivity(activity.Activity):
         self.notify_alert()
 
     def write(self, icon):
-        client = GConf.Client.get_default()
-        path = "/desktop/sugar/user/icon"
-        client.set_string(path, icon)
+        root_path = os.path.join(os.path.expanduser('~'), '.icons')
+        to_path = os.path.join(root_path, SUGAR_ICON_PATH,
+                               DEFAULT_ICON + '.svg')
+        if icon == DEFAULT_ICON:
+            command = ['rm', to_path]
+        else:
+            from_path = os.path.join(root_path, icon + '.svg') 
+            command = ['cp', from_path, to_path]
+        subprocess.check_output(command)
         return True
 
     def notify_alert(self):
